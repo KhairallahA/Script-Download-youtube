4cec288a871a8447bd8c360df59f2570ff2a6407
Khairallah AL-Awady <71499890+KhairallahA@users.noreply.github.com>
2020-12-04 19:00:58 +0300

    Update and rename Test.py to Script.py

--- Script.py
+++ Script.py
@@ -3,15 +3,13 @@
 # test = new('https://www.youtube.com/watch?v=8ijsxKzgROM')
 # print(test)
 
-# https://www.youtube.com/watch?v=dNxNU-SPUW0
-
 # لتحميل الفيديو بالدقة التي اريدها
 # from pafy import new
 # url = input('Enter you link here: ')
 # video = new(url)
 # stream = video.streams
-for i in stream:
-    print(i)
+# for i in stream:
+#     print(i)
 # stream[0].download()
 
 # لتحميل الفيديو باقوى دقة بدون إختيار
